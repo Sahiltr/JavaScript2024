ad6c693de9deb604d7f234ada874c6f23ce4bd1f
Sahil Thakur <thakursahil1912@gmail.com>
2024-03-24 13:32:22 +0530

    .

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,19 +1,46 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
 
 // "33" => 33
 //"33abc" => NaN 
 //true => 1, false =>0
 
-let isLoggedIn = 1
-let boolValue = Boolean(isLoggedIn)
-console.log(boolValue);
+// let isLoggedIn = 1
+// let boolValue = Boolean(isLoggedIn)
+// console.log(boolValue);
 
 //1 => true, 0=> false
 // "" => true
 //"absd" => true
 
+
+//******************************************Operations***************************************//
+// let num = 33
+// let stringnum = String(num)
+// console.log(typeof num);
+// console.log(typeof stringnum);
+
+// let negvalue =-num
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " sahil"
+let str3 = str1+str2
+
+console.log(str3);
+
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+ "2");
