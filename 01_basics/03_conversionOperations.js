6819fa5f180f7a80dfb113d0a5cb8be4bd5ff34a
Sahil Thakur <thakursahil1912@gmail.com>
2024-03-24 10:42:17 +0530

    .

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN 
+//true => 1, false =>0
+
+let isLoggedIn = 1
+let boolValue = Boolean(isLoggedIn)
+console.log(boolValue);
+
+//1 => true, 0=> false
+// "" => true
+//"absd" => true
+
